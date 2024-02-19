1a7bf50f59741d0b2f27f344f6c08624fb8751aa
rigsel02230103 <02230103.cst@rub.edu.bt>
2024-02-19 14:30:44 -0500

    update shh.py

---
+++ shh.py
@@ -0,0 +1,13 @@
+grade = 93
+
+if grade >= 90:
+    print("Ekdam jakas")
+elif grade >= 80:
+    print("Jachichi")
+elif grade >= 70:
+    print("Tubay maena")
+elif grade >= 60:
+    print("haha hanging mae")
+else:
+    print("Go to arts")
+    
