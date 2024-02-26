375110305eb496da6193a3138d8983d0fd8c5090
rigsel02230103 <02230103.cst@rub.edu.bt>
2024-02-26 15:01:04 -0500

    Update logical.py

---
+++ logical.py
@@ -0,0 +1,24 @@
+#Example 1
+x = 5
+print(x > 3 and x < 10) #True because both conditions are True
+
+#Example 2
+y = 12
+print(y > 10 and y % 5== 0) #False because the second condition is False
+
+#Example 3
+q = 5
+print(q < 5 or q > 10) #False because both conditions are False
+
+#Example 4
+w = 12
+print(w < 10 or w % 2 == 0) #True because the second condition is True
+
+#Example 5 
+r = 5
+print(not(r > 3 and r < 10)) #False because the conditions inside the not is True
+
+
+#Example 6
+g = 12
+print(not(g > 10 and g % 5 == 0)) #True because the conditions inside the not is False# Example 1
